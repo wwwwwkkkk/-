98b2e57b9c7acb25d716779a16c4599e4eb0f026
WWWWWKKKK <459973981@qq.com>
2023-03-29 00:53:07 +0800

    first commit

---
+++ Demo1.java
@@ -0,0 +1,19 @@
+public class Demo1 {
+    public static void main(String[] args) {
+
+        //数据类型 [] 数组名 = {}
+        int [] arr = {1,2,3,4,5,6,7,8,9,10};
+
+        for (int i = 0; i < arr.length ; i++) {
+
+            if(arr[i] % 2 == 0){
+
+            }
+        }
+
+
+
+
+    }
+
+}
